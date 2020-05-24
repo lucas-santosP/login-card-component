82cbc51f8e881c19fd83df67f32e710c975cd809
lucas-santosP <lucaspolicarpo.2018@gmail.com>
2020-05-24 18:28:25 -0300

    initial test examples

--- __tests__/example.spec.js
+++ __tests__/example.spec.js
@@ -1,12 +1,26 @@
-import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
+//FontAwesome
+import {
+  faSync,
+  faUser,
+  faEnvelope,
+  faEye,
+  faEyeSlash,
+} from "@fortawesome/free-solid-svg-icons";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+library.add(faSync, faEnvelope, faUser, faEye, faEyeSlash);
+import Vuelidate from "vuelidate";
 
-describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg }
-    })
-    expect(wrapper.text()).toMatch(msg)
-  })
-})
+import { shallowMount } from "@vue/test-utils";
+import SignIn from "@/components/SignIn.vue";
+import Vue from "vue";
+Vue.use(Vuelidate);
+Vue.component("font-awesome-icon", FontAwesomeIcon);
+
+describe("HelloWorld.vue", () => {
+  it("check if SignIn is a vue component", () => {
+    const wrapper = shallowMount(SignIn);
+
+    expect(wrapper.isVueInstance).toBeTruthy;
+  });
+});
